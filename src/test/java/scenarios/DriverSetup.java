b60c92fed5a751296b329dcb153bd0b0b55b1c75
mukhlygina <m.mukhlygina@gmail.com>
2018-02-09 15:35:02 +0300

    Initial commit

---
+++ src/test/java/scenarios/DriverSetup.java
@@ -0,0 +1,35 @@
+package scenarios;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class DriverSetup {
+    protected AndroidDriver driver;
+
+    protected void prepareNative() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName","emulator-5554");
+        capabilities.setCapability("platformName","Android");
+
+        String file = getClass().getClassLoader().getResource("ContactManager.apk").getFile();
+        File app = new File(file);
+
+        capabilities.setCapability("app", app.getAbsolutePath());
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    protected void prepareAndroidWeb() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName","emulator-5554");
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("browserName", "Chrome");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+    }
+}
---
+++ src/test/java/scenarios/SimpleTest.java
@@ -0,0 +1,35 @@
+package scenarios;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+
+public class SimpleTest extends DriverSetup {
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        prepareNative();
+//        prepareAndroidWeb();
+    }
+
+    @Test
+    public void simpleTest(){
+        String app_package_name = "com.example.android.contactmanager:id/";
+        driver.findElement(By.id(app_package_name + "addContactButton")).click();
+        System.out.println("Simplest Appium test done");
+    }
+
+    @Test
+    public void webTest() throws InterruptedException {
+        driver.get("http://iana.org");
+        Thread.sleep(5000);
+        System.out.println("Site opening done");
+    }
+
+    @AfterClass
+    public void tearDown() throws MalformedURLException {
+        driver.quit();
+    }
+}
